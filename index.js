6d5d45e6a498242b845aec8ad07f3381e6f1699c
Bruno Capobianco <bruno.capobianco@outlook.it>
2023-10-10 17:12:27 +0200

    some bug fixed

--- index.js
+++ index.js
@@ -25,3 +25,26 @@ const cancella=() =>
 }
 save.addEventListener("click",salvataggio)
 deleter.addEventListener("click", cancella)
+
+const localStorageKey1="time_memory"
+let counter=document.getElementById("second")
+let test=sessionStorage.getItem(localStorageKey1)
+let seconds=0
+if(isNaN(test))
+{
+    seconds=0
+    setInterval(incrementSeconds,1000)
+}
+else
+{
+    seconds=parseInt(test)
+    setInterval(incrementSeconds,1000)
+}
+
+function incrementSeconds()
+{
+    seconds += 1;
+    sessionStorage.setItem(localStorageKey1,seconds)
+    counter.innerText = "You have been here for " + seconds + " seconds." ;
+}
+    
